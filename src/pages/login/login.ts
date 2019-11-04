a6fe5fbb789a488132f2c090036a3aa048a5f607
MisaelKelviny <misaelmbass@gmail.com>
2019-11-03 23:29:52 -0300

    tentando adicionar o pwa button na tela inicial

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -21,7 +21,7 @@ export class LoginPage {
   login: string = "";
   userName: string = "";
   showBtn: boolean = false;
-  deferredPrompt;
+  deferredPrompt: any;
   private _userLogged: any;
 
   constructor(
@@ -121,32 +121,23 @@ export class LoginPage {
   ionViewWillEnter() {
     this.load.hide();
     window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later on the button event.
+      // e.preventDefault();
+      this.add_to_home(e)
       this.deferredPrompt = e;
-
-      // Update UI by showing a button to notify the user they can add to home screen
       this.showBtn = true;
     });
-
-    //button click event to show the promt
-
     window.addEventListener('appinstalled', (event) => {
       console.log('installed');
     });
-
-
     if (window.matchMedia('(display-mode: standalone)').matches) {
       console.log('display-mode is standalone');
     }
   }
 
   add_to_home(e) {
-    // hide our user interface that shows our button
-    // Show the prompt
-    this.deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
+    if(this.deferredPrompt){
+      this.deferredPrompt.prompt();
+    }
     this.deferredPrompt.userChoice
       .then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
